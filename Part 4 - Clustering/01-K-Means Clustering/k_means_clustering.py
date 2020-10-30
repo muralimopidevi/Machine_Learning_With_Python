f6e7aa8e6c171ebb21f0daf3947110d61ca0a037
Murali Krishna Mopidevi <muralikrishna.mopidevi@gmail.com>
2020-10-30 10:47:24 +0100

    classification and clustering upd

--- Part 4 - Clustering/01-K-Means Clustering/k_means_clustering.py
+++ Part 4 - Clustering/01-K-Means Clustering/k_means_clustering.py
@@ -8,19 +8,6 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
-# y = dataset.iloc[:, 3].values
-
-# Splitting the dataset into the Training set and Test set
-"""from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
-
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
 
 # Using the elbow method to find the optimal number of clusters
 from sklearn.cluster import KMeans
@@ -35,7 +22,7 @@ plt.xlabel('Number of clusters')
 plt.ylabel('WCSS')
 plt.show()
 
-# Fitting K-Means to the dataset
+# Training the K-Means model on the dataset
 kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 42)
 y_kmeans = kmeans.fit_predict(X)
 
